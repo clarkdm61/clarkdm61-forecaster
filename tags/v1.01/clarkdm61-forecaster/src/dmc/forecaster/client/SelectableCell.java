7c389471b722b2e3931fce7b9b342b28021bec91
clarkdm61 <clarkdm61@b2c88cb1-a9f6-f54e-e177-533e194cbd8d>
2010-11-04 21:39:51 +0000

    tag before fixing date format

---
+++ tags/v1.01/clarkdm61-forecaster/src/dmc/forecaster/client/SelectableCell.java
@@ -0,0 +1,56 @@
+package dmc.forecaster.client;
+
+import java.util.Date;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Label;
+
+import dmc.forecaster.shared.FinancialEvent;
+
+public class SelectableCell extends Label {
+	private FinancialEvent financialEvent = null;
+	private static long currTimeAdjusted = 0;
+	static {
+		Date adjDt = new Date(System.currentTimeMillis());
+		adjDt.setYear(adjDt.getYear()-100);
+		currTimeAdjusted = adjDt.getTime();
+	}
+
+	public SelectableCell(FinancialEvent financialEvent, final ManageTab manageTab) {
+		setFinancialEvent(financialEvent);
+		setText(financialEvent.getLabelString());
+		
+		if (financialEvent.getEndDt() != null && financialEvent.getEndDt().getTime() < currTimeAdjusted) {
+			this.addStyleName("oldItem");
+		}
+		
+		addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				if (manageTab.getSelectedCell() == SelectableCell.this) {
+					// same one - just deselect
+					manageTab.getSelectedCell().removeStyleName("selectedItem");
+					manageTab.setSelectedCell(null);
+				} else {
+					// change selection 
+					SelectableCell.this.addStyleName("selectedItem");
+					if (manageTab.getSelectedCell()!=null) manageTab.getSelectedCell().removeStyleName("selectedItem");
+					manageTab.setSelectedCell(SelectableCell.this);
+				}
+			}
+		});
+	}
+
+
+	public FinancialEvent getFinancialEvent() {
+		return financialEvent;
+	}
+
+
+	public void setFinancialEvent(FinancialEvent financialEvent) {
+		this.financialEvent = financialEvent;
+	}
+
+}
