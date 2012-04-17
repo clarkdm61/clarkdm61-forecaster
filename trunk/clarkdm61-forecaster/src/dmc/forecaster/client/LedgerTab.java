688ea2c95a962f9f797a4e621b10d37db4067e81
clarkdm61 <clarkdm61@b2c88cb1-a9f6-f54e-e177-533e194cbd8d>
2012-04-17 21:18:59 +0000

    refactored to remove ledger list code from UI

--- trunk/clarkdm61-forecaster/src/dmc/forecaster/client/LedgerTab.java
+++ trunk/clarkdm61-forecaster/src/dmc/forecaster/client/LedgerTab.java
@@ -3,7 +3,10 @@ package dmc.forecaster.client;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
+import com.example.vaadingae.LedgerEntryFactory;
+import com.example.vaadingae.VaadingaeApplication;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -111,21 +114,11 @@ public class LedgerTab extends DockLayoutPanel {
 	 * @param endDt
 	 */
 	private void createLedger(String startDt, String endDt) {
-		ledgerEntries = new ArrayList<LedgerEntry>();
 		
-		// for each FinancialEvent..
-		// create instances within date range
-		for (FinancialEvent event : Clarkdm61_forecaster.manageTab.getEventList()) {
-			// handle reoccurring events differently
-			if (!event.getReoccurrence().equals(Reoccurrence.None)) {
-				createLedgerEntries(ledgerEntries, event); 
-			} else {
-				createLedgerEntry(ledgerEntries, event);
-			}
-		}
+		List<FinancialEvent> financialEvents = VaadingaeApplication.getFinancialEventList();
+		LedgerEntryFactory factory = new LedgerEntryFactory();
 		
-		// sort entries by start date (note: the balance can't be calculated until this happens)
-		Collections.sort(ledgerEntries);
+		ledgerEntries = factory.createLedgerEntries(financialEvents, getStartDt(), getEndDt());
 		
 		// update ui, and calculate balance while enumerating entries
 		Runnable runnableCallBack = new Runnable() {
@@ -211,105 +204,6 @@ public class LedgerTab extends DockLayoutPanel {
 		return options;
 	}
 	
-	/**
-	 * Update legerEntries from the reoccurring event within user-specified range 
-	 * @param ledgerEntries
-	 * @param event
-	 */
-	private void createLedgerEntries(ArrayList<LedgerEntry> ledgerEntries, FinancialEvent event) {
-		// if the start date is future, or the end date is past; just stop.
-		if (isDateGreaterThanEnd(event.getStartDt()) || isDateLessThanStart(event.getEndDt())) {
-			return;
-		}
-		Date instanceDate = event.getStartDt();
-		// we know it's in range, so make instances
-		do {
-			// is date in range of ledger (could be too soon)?
-			if (isDateGreaterThanOrEqualToStart(instanceDate)) {
-				// add to ledger
-				LedgerEntry entry = new LedgerEntry(event.getName(), event.getType(), event.getAmount(), instanceDate);
-				// highlight unusual events
-				if (event.getReoccurrence().equals(Reoccurrence.TwiceYearly)
-						|| event.getReoccurrence().equals(Reoccurrence.Yearly)
-						) {
-					entry.setRowColor("blue"); // TODO: make colors constants
-				}
-				ledgerEntries.add(entry);
-			} 
-			// increment
-			instanceDate = event.getReoccurrence().getNext(instanceDate);
-			// stop if incremented instanceDate exceeds ledger end date, or the event's end date
-		} while (isDateLessThanOrEqualToEnd(instanceDate) && event.isInDateRange(instanceDate));
-	}
-
-	/**
-	 * If event's date is in range, create and add a ledger instance
-	 * @param ledgerEntries
-	 * @param event
-	 */
-	private void createLedgerEntry(
-			ArrayList<LedgerEntry> ledgerEntries, FinancialEvent event) {
-		if (isDateInLedgerRange(event.getStartDt())) {
-			LedgerEntry entry = new LedgerEntry(event.getName(), event.getType(), event.getAmount(), event.getStartDt());
-			entry.setRowColor("blue");// TODO: make colors constants
-			ledgerEntries.add(entry);	
-		}
-	}
-	
-	/**
-	 * @param aDate
-	 * @return true if specified date is between start and end dates (inclusive)
-	 */
-	private boolean isDateInLedgerRange(Date aDate) {
-		if (aDate == null) {
-			// if it's null, it's an endDate, which is always in range
-			return true;
-		} else {
-			return isDateGreaterThanOrEqualToStart(aDate) && isDateLessThanOrEqualToEnd(aDate);
-		}
-	}
-	
-	/**
-	 * Returns true if aDate >= Ledger Start Date
-	 * @param aDate
-	 * @return
-	 */
-	private boolean isDateGreaterThanOrEqualToStart(Date aDate) {
-		return aDate.equals(getStartDt()) || aDate.after(getStartDt());
-	}
-	
-	/**
-	 * Returns true if aDate <= Ledger End Date
-	 * @param aDate
-	 * @return
-	 */
-	private boolean isDateLessThanOrEqualToEnd(Date aDate) {
-		if (aDate == null) {
-			// if it's null, it's an endDate, which is always in range
-			return true;
-		} else {
-			return aDate.equals(getEndDt()) || aDate.before(getEndDt());
-		}
-	}
-	
-	/**
-	 * Returns true if [start]aDate >= Ledger End Date
-	 */
-	private boolean isDateGreaterThanEnd(Date aDate) {
-		return aDate.after(getEndDt());
-	}
-	
-	/**
-	 * Returns true if [end]aDate < Ledger Start Date
-	 */
-	private boolean isDateLessThanStart(Date aDate) {
-		if (aDate == null) {
-			return false;
-		} else {
-			return aDate.before(getStartDt());
-		}
-	}
-		
 	@SuppressWarnings("deprecation")
 	private Date getStartDt() {
 		return new Date(txtStart.getText());
