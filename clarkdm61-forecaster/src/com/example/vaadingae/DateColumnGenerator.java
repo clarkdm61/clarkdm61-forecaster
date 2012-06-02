15f167c39156cbffb0389a91a0bf3049c03bb152
clarkdm61@gmail.com <clarkdm61@gmail.com@b2c88cb1-a9f6-f54e-e177-533e194cbd8d>
2012-06-02 01:59:01 +0000

    Fix for Defect 18

---
+++ clarkdm61-forecaster/src/com/example/vaadingae/DateColumnGenerator.java
@@ -0,0 +1,37 @@
+package com.example.vaadingae;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.vaadin.data.Property;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.Table.ColumnGenerator;
+
+/**
+ * Required to specify date format
+ * @author David
+ *
+ */
+public class DateColumnGenerator implements ColumnGenerator {
+
+	SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+	
+	@Override
+	public Object generateCell(Table source, Object itemId, Object columnId) {
+		 // Get the object stored in the cell as a property
+        Property prop =
+            source.getItem(itemId).getItemProperty(columnId);
+        if (prop.getType().equals(Date.class)) {
+            
+        	Date date = (Date) prop.getValue(); 
+        	
+        	Label label = new Label(df.format(date));
+
+            return label;
+        }
+        return null;
+	}
+
+}
