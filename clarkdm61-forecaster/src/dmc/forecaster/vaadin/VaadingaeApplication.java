3d202ca4c5bebea639f044aae9f29ee8f689f514
clarkdm61 <clarkdm61@b2c88cb1-a9f6-f54e-e177-533e194cbd8d>
2012-08-23 15:30:45 +0000

    Add AGPL 3.0

--- clarkdm61-forecaster/src/dmc/forecaster/vaadin/VaadingaeApplication.java
+++ clarkdm61-forecaster/src/dmc/forecaster/vaadin/VaadingaeApplication.java
@@ -1,5 +1,25 @@
 package dmc.forecaster.vaadin;
 
+/**
+  	The Forecaster application is a financial assistance helper. It allows 
+  	a user to create, edit, delete Financial Events that may be one-time and 
+  	reoccurring Income or Expense events. These events can then be used to 
+  	generate projections in either a ledger, or graphical format.
+    Copyright (C) 2010-2012  David Clark
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import java.io.Serializable;
 
 import com.vaadin.Application;
