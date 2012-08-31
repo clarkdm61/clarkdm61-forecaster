8c9b0d660927b9655d0cd668c41ce2e4baf77a00
clarkdm61 <clarkdm61@b2c88cb1-a9f6-f54e-e177-533e194cbd8d>
2012-08-31 21:00:51 +0000

    format

--- trunk/clarkdm61-forecaster/src/dmc/forecaster/vaadin/AppData.java
+++ trunk/clarkdm61-forecaster/src/dmc/forecaster/vaadin/AppData.java
@@ -14,68 +14,65 @@ import dmc.forecaster.shared.FinancialEvent;
 
 /**
  * The pattern for using AppData is analogous to Session data for a Vaadin app.
- * The basis for the datastructure, use of ThreadLocal, and initialization comes directly
- * from the Book of Vaadin.
+ * The basis for the datastructure, use of ThreadLocal, and initialization comes
+ * directly from the Book of Vaadin.
  * 
  * @author David
- *
+ * 
  */
 public class AppData implements TransactionListener, Serializable {
-	
+
 	private static final long serialVersionUID = 9027135772079670233L;
 	private Application app;
 	private static ThreadLocal<AppData> instance = new ThreadLocal<AppData>();
 	private List<FinancialEvent> financialEventList = null;
 	private ArrayList<LedgerEntry> ledgerEntries = null;
 	private ForecasterService forecasterService = null;
-	
 
-	 public AppData(Application app) {
-	        this.app = app;
+	public AppData(Application app) {
+		this.app = app;
 
-	        // It's usable from now on in the current request
-	        instance.set(this);
-	    }
+		// It's usable from now on in the current request
+		instance.set(this);
+	}
 
-	    @Override
-	    public void transactionStart(Application application,
-	                                 Object transactionData) {
-	        // Set this data instance of this application
-	        // as the one active in the current thread. 
-	        if (this.app == application)
-	            instance.set(this);
-	    }
+	@Override
+	public void transactionStart(Application application, Object transactionData) {
+		// Set this data instance of this application
+		// as the one active in the current thread.
+		if (this.app == application)
+			instance.set(this);
+	}
 
-	    @Override
-	    public void transactionEnd(Application application,
-	                               Object transactionData) {
-	        // Clear the reference to avoid potential problems
-	        if (this.app == application)
-	            instance.set(null);
-	    }
+	@Override
+	public void transactionEnd(Application application, Object transactionData) {
+		// Clear the reference to avoid potential problems
+		if (this.app == application)
+			instance.set(null);
+	}
 
-		public static List<FinancialEvent> getFinancialEventList() {
-			return instance.get().financialEventList;
+	public static List<FinancialEvent> getFinancialEventList() {
+		return instance.get().financialEventList;
+	}
+
+	public static ForecasterService getForecasterService() {
+		if (instance.get().forecasterService == null) {
+			instance.get().forecasterService = new ForecasterServiceImpl();
 		}
-		
-		public static ForecasterService getForecasterService() {
-			if (instance.get().forecasterService == null) {
-				instance.get().forecasterService = new ForecasterServiceImpl();
-			}
-			return instance.get().forecasterService;
-		}
+		return instance.get().forecasterService;
+	}
 
-		public static void setFinancialEventList(List<FinancialEvent> financialEventList) {
-			instance.get().financialEventList = financialEventList;
-		}
+	public static void setFinancialEventList(
+			List<FinancialEvent> financialEventList) {
+		instance.get().financialEventList = financialEventList;
+	}
 
-		public static ArrayList<LedgerEntry> getLedgerEntries() {
-			return instance.get().ledgerEntries;
-		}
+	public static ArrayList<LedgerEntry> getLedgerEntries() {
+		return instance.get().ledgerEntries;
+	}
 
-		public static void setLedgerEntries(ArrayList<LedgerEntry> ledgerEntries) {
-			instance.get().ledgerEntries = ledgerEntries;
-		}
-
+	public static void setLedgerEntries(ArrayList<LedgerEntry> ledgerEntries) {
+		instance.get().ledgerEntries = ledgerEntries;
+	}
 
 }
